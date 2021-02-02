c4ed92d5cbf777a320880fc4be31b8149c998f56
Kunal Kashyap <34695022+kunalkashyap855@users.noreply.github.com>
2021-02-03 01:19:58 +0530

    peerjs server fix

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: 'arcane-peak-74802.herokuapp.com',
-  port: '9000'
+  port: '443'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
