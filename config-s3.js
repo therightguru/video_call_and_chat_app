72c3e30aca50b3c94269b87e7573052cbf079938
Sachin Saini <sainisachin1906@gmail.com>
2021-05-24 23:06:52 +0530

    config

---
+++ config-s3.js
@@ -0,0 +1,4 @@
+module.exports.accessKeyId = "AKIAZI2IEUR44S67AS2E"; 
+module.exports.secretAccessKey = "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"; 
+module.exports.bucketName = "therightguru-website"; 
+module.exports.region = "ap-south-1"; 
