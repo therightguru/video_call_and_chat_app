250d29360ddf3f3f9e60c1b25a83b9c0972d645c
Sachin Saini <sainisachin1906@gmail.com>
2021-05-29 16:21:56 +0530

    config delete

--- config-s3.js
+++
@@ -1,4 +0,0 @@
-module.exports.accessKeyId = "AKIAZI2IEUR44S67AS2E"; 
-module.exports.secretAccessKey = "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"; 
-module.exports.bucketName = "therightguru-website"; 
-module.exports.region = "ap-south-1"; 
